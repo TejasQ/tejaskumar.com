fb5232b24a1772436603c2ee2ffc6d47eec2ebde
Lucas Mancini <mancini.lucas@gmail.com>
2020-04-11 21:56:07 +1000

    Use an API route intead of a React component.

--- pages/api/rss.ts
+++ pages/api/rss.ts
@@ -1,14 +1,12 @@
-import React from "react";
-import { NextPageContext } from "next";
-import { title } from "case";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { title } from 'case';
 
-import { getInitialBlogPosts, Post } from "../util/getInitialBlogPosts";
+import { getInitialBlogPosts, Post } from '../../util/getInitialBlogPosts';
 
 const getRssXml = (blogPosts: Post[]) => {
-  let rssItemsXml = "";
-  blogPosts.forEach(post => {
-    const permalink = `https://tejaskumar.com/blog/${post.slug}`
-    rssItemsXml += `
+  const rssItemsXml = blogPosts.map(post => {
+    const permalink = `https://tejaskumar.com/blog/${post.slug}`;
+      return `
       <item>
         <title>${title(post.body.split("\n")[0].replace("# ", ""))}</title>
         <link>${permalink}</link>
@@ -20,6 +18,7 @@ const getRssXml = (blogPosts: Post[]) => {
         <guid>${permalink}</guid>
     </item>`;
   });
+
   return `<?xml version="1.0" ?>
     <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
@@ -33,21 +32,13 @@ const getRssXml = (blogPosts: Post[]) => {
     </rss>`;
 };
 
-class Rss extends React.Component {
-  static async getInitialProps({ res }: NextPageContext) {
-    if (!res) {
-      return;
-    }
-    const { posts } = await getInitialBlogPosts();
-    res.setHeader("Content-Type", "text/xml");
-    res.write(getRssXml(posts));
-    res.end();
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
+  if (!res) {
+    return;
   }
+  const { posts } = await getInitialBlogPosts();
 
-  render() {
-    return null;
-  }
+  res.setHeader("Content-Type", "text/xml");
+  res.write(getRssXml(posts));
+  res.end();
 }
-
-
-export default Rss;
