1e95fb703d2b46b78926b6a95ff98cf7620f0c4c
Tejas Kumar <tejas@tejas.qa>
2022-05-28 03:17:14 +0200

    Adjust tweets and stuff

--- create-testimonials.ts
+++ create-testimonials.ts
@@ -34,14 +34,14 @@ const main = async () => {
         if (howManyMoreCanIDo <= 1) {
             const howLongToWait = rateLimitResetTime ? differenceInMilliseconds(parseFloat(rateLimitResetTime) * 1000, new Date()) : 900001;
             console.info(`We're about to be rate limited. Waiting ${howLongToWait / 1000} seconds...`);
-            console.info("Users so far", testimonialsFromXata.length, 'of', users.length);
+            console.info("Users so far", (await client.db.testimonials.getAll()).length, 'of', users.length);
             await wait(howLongToWait);
-            howManyMoreCanIDo = 900;
+            howManyMoreCanIDo = 300;
         }
 
         if (!testimonialsFromXata.find(t => t.tweet_url === testimonials[userIndex])) {
             console.log(`Couldn't find ${users[userIndex]}. Fetching from Twitter...`);
-            console.info(`We can do ${howManyMoreCanIDo} more requests at this time.`);
+            console.info(`We can do ${howManyMoreCanIDo} more requests at this time. (${users.length} total)`);
             await fetchFromTwitter(users[userIndex]).then(async (u) => {
                 if (u.errors) {
                     return;
